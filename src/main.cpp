f09cd42d3fc306684f4a2a132c9dce7283e8b46b
Filip Toloczko <ftolo2@uic.edu>
2026-04-22 17:24:43 -0500

    working on email

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,53 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <ESP_Mail_Client.h>
 #include <secrets.h>
 
-// put function declarations here:
-int myFunction(int, int);
+SMTPSession smtp;
 
-void setup() {
+// put function declarations here:
+void sendEmail(const char *, const char *, const char *);
+
+void setup()
+{
   // put your setup code here, to run once:
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void sendEmail(const char *recipient, const char *subject, const char *body)
+{
+  ESP_Mail_Session session;
+  session.server.host_name = "smpt.gmail.com";
+  session.server.port = 465;
+  session.login.email = SENDER_EMAIL;
+  session.login.password = SENDER_PASSWORD;
 
+  SMTP_Message message;
+  message.sender.name = "ESP32";
+  message.sender.email = SENDER_EMAIL;
+  message.subject = subject;
+  message.addRecipient("Recipient", recipient);
+  message.text.content = body;
+
+  if (!smtp.connect(&session))
+  {
+    Serial.print("Connection failed: ");
+    Serial.println(smtp.errorReason());
+    return;
+  }
+
+  if (!MailClient.sendMail(&smtp, &message))
+  {
+    Serial.print("Send failed: ");
+    Serial.println(smtp.errorReason());
+    return;
+  }
+
+  Serial.println("Email sent");
+}
