89c33992cc000937ade342c3a534c11103ba917d
Filip Toloczko <ftolo2@uic.edu>
2026-04-22 16:58:28 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <secrets.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
+
