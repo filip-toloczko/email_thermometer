a76066f9f144ad623308bdf59323cb60865357c4
Filip Toloczko <ftolo2@uic.edu>
2026-04-22 23:15:22 -0500

    working on temperature

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,41 @@
 #include <Arduino.h>
+#include <string>
 #include <WiFi.h>
 #include <ESP_Mail_Client.h>
+#include <DHT.h>
 #include <secrets.h>
 
+#define DHT_PIN 4
+#define DHT_TYPE DHT11
+
+using namespace std;
+
 SMTPSession smtp;
+DHT dht(DHT_PIN, DHT_TYPE);
 
 // put function declarations here:
-void sendEmail(const char *, const char *, const char *);
+void sendEmail(const char *recipient, const char *subject, const char *body);
 void connectWifi();
+void checkTemp();
 
 void setup()
 {
   Serial.begin(115200);
   connectWifi();
-
-  sendEmail(
-    RECIPIENT1,
-    "Hello",
-    "This is a test email"
-  );
+  dht.begin();
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  checkTemp();
 }
 
 void connectWifi()
 {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -68,4 +73,30 @@ void sendEmail(const char *recipient, const char *subject, const char *body)
   }
 
   Serial.println("Email sent");
+}
+
+void checkTemp()
+{
+  delay(2000);
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature(true);
+
+  if (isnan(humidity) || isnan(temperature))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  if (temperature >= 40.0)
+  {
+    String message = "WARNING: Fridge Temperature Below 40 °F\n";
+    message += "Current Fridge Temperature: ";
+    message += String(temperature);
+    message += " °F";
+
+    String subject = "WARNING: Garage Fridge Temperature Low";
+
+    sendEmail(RECIPIENT1, subject.c_str(), message.c_str());
+  }
 }
